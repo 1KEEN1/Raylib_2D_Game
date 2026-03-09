c8025537418429bec6c091dd9ae133d0efd8e69f
KEEN <rpirakov20@gmail.com>
2026-03-09 15:30:14 +0700

    Test app using raylib.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
-#include <iostream>
+#include "raylib.h"
 
 int main() {
-	std::cout << "Hello, World" << std::endl;
+    InitWindow(800, 450, "Raylib Test");
+    SetTargetFPS(60);
 
-	return 0;
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Hello, World!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
